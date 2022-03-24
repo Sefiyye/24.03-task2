8ff66cc9f4f3bcfa7d8c9131c05d8a06f4dafe1b
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-24 10:00:21 +0400

    Task

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extension_task1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static string CustomContains(this string str)
+        {
+            return str;
+        }
+    }
+}
